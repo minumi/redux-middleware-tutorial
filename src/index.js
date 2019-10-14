f933a73c7511d7490a65e2b62d0445d03b6b772e
minumi <kimkimee2@gmail.com>
2019-10-14 20:28:16 +0900

    Async로 변경

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+// import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
